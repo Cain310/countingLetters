c7e8aab8f633f94eaa59357549455e49cda71c65
Michael Cain <michaelcaincb@gmail.com>
2019-04-03 16:18:22 +0000

    fixed indentation

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,13 @@
 function countLetters(str) {
     str = str.toLowerCase().split(' ').join('');
-
     var letters = {}
+
     for (var i = 0; i < str.length; i++) {
 
         if (letters[str[i]] === undefined)
             letters[str[i]] = 1;
         else
             letters[str[i]] += 1;
-
-
-        //str[0] = l
     }
     return letters;
 }
